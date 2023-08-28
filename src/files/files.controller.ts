e0f506f3966405f393bc8e992a59c8c63c181fe0
Brian Ramirez <ramirez7358@gmail.com>
2023-08-27 22:00:44 -0300

    Create files endpoint with interceptor

---
+++ src/files/files.controller.ts
@@ -0,0 +1,23 @@
+import {
+  BadRequestException,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FilesService } from './files.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helpers/file-filter.helper';
+
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors(FileInterceptor('file', { fileFilter: fileFilter }))
+  uploadFiles(@UploadedFile() file: Express.Multer.File) {
+    if (!file)
+      throw new BadRequestException('Make sure that the file is an image');
+    return { fileName: file.originalname };
+  }
+}
---
+++ src/files/helpers/file-filter.helper.ts
@@ -0,0 +1,15 @@
+export const fileFilter = (
+  req: Express.Request,
+  file: Express.Multer.File,
+  callback: Function,
+) => {
+  if (!file) callback(new Error('File is empty'), false);
+
+  const fileExtension = file.mimetype.split('/')[1];
+  const validExtensions = ['jpg', 'jpeg', 'png'];
+
+  if (validExtensions.includes(fileExtension)) {
+    return callback(null, true);
+  }
+  callback(null, false);
+};
