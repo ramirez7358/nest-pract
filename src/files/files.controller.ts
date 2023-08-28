8eb46b7ccd62db99bea2db8d15f3b5a870bd104b
Brian Ramirez <ramirez7358@gmail.com>
2023-08-27 22:44:00 -0300

    Upload files in disk storage

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,23 +1,54 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { fileFilter } from './helpers/file-filter.helper';
+import { diskStorage } from 'multer';
+import { fileFilter, fileNamer } from './helpers';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Get('products/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+    res.sendFile(path);
+  }
 
   @Post('product')
-  @UseInterceptors(FileInterceptor('file', { fileFilter: fileFilter }))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      storage: diskStorage({
+        destination: './static/uploads',
+        filename: fileNamer,
+      }),
+    }),
+  )
   uploadFiles(@UploadedFile() file: Express.Multer.File) {
     if (!file)
       throw new BadRequestException('Make sure that the file is an image');
-    return { fileName: file.originalname };
+
+    const secureUrl = `${this.configService.get('HOST_API')}/files/products/${
+      file.filename
+    }`;
+
+    return { secureUrl };
   }
 }
