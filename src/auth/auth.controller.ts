4587c8e96fcc555b7376ce39e84abffcef882c77
Brian Ramirez <ramirez7358@gmail.com>
2023-09-08 22:15:26 -0300

    Add custom decorators to get headers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Post, Body, Get, UseGuards, Req } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  UseGuards,
+  Req,
+  Headers,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
-import { GetUser } from './decorators/get-user.decorator';
 import { User } from './entities/user.entity';
+import { RawHeaders, GetUser } from './decorators';
+import { IncomingHttpHeaders } from 'http2';
 
 @Controller('auth')
 export class AuthController {
@@ -21,7 +30,11 @@ export class AuthController {
 
   @Get('private')
   @UseGuards(AuthGuard())
-  testingPrivateRoute(@GetUser('email') user: User) {
+  testingPrivateRoute(
+    @GetUser() user: User,
+    @RawHeaders() rawHeaders: string[],
+    @Headers() headers: IncomingHttpHeaders,
+  ) {
     return 'Ok!';
   }
 }
