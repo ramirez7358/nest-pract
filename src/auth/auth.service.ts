11837831bc1c84a49fb7eff51415bdcf0ad633d8
Brian Ramirez <ramirez7358@gmail.com>
2023-09-08 21:51:22 -0300

    Change payload

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -31,7 +31,7 @@ export class AuthService {
       await this.userRepository.save(user);
 
       delete user.password;
-      return { ...user, token: this.getJwtToken({ email: user.email }) };
+      return { ...user, token: this.getJwtToken({ id: user.id }) };
     } catch (error) {
       this.handleDBException(error);
     }
@@ -41,7 +41,7 @@ export class AuthService {
     const { password, email } = loginUserDto;
     const user = await this.userRepository.findOne({
       where: { email },
-      select: { email: true, password: true },
+      select: { email: true, password: true, id: true }, //! OJO
     });
 
     if (!user)
@@ -49,7 +49,7 @@ export class AuthService {
 
     if (!bcrypt.compareSync(password, user.password))
       throw new UnauthorizedException('Credentials are not valid (email) ');
-    return { ...user, token: this.getJwtToken({ email: user.email }) };
+    return { ...user, token: this.getJwtToken({ id: user.id }) };
   }
 
   private getJwtToken(payload: JwtPayload) {
