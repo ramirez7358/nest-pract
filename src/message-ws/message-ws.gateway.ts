332abab619f99c42c2d9a8ec0a31b5dfb2515c37
Brian Ramirez <ramirez7358@gmail.com>
2023-09-15 00:39:14 -0300

    Add connection counter

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -13,10 +13,13 @@ export class MessageWsGateway
   constructor(private readonly messageWsService: MessageWsService) {}
 
   handleConnection(client: Socket, ...args: any[]) {
-    console.log('Cliente conectado:', client.id);
+    this.messageWsService.registerClient(client);
+
+    console.log({ conectados: this.messageWsService.getConnectedClients() });
   }
 
   handleDisconnect(client: Socket) {
-    console.log('Cliente desconectado', client.id);
+    this.messageWsService.removeClient(client.id);
+    console.log({ conectados: this.messageWsService.getConnectedClients() });
   }
 }
