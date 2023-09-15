613a64ce5b2ea622d2e476feab893dc52c91a519
Brian Ramirez <ramirez7358@gmail.com>
2023-09-15 00:12:54 -0300

    Handle connection and desconetion

--- src/message-ws/message-ws.gateway.ts
+++ src/message-ws/message-ws.gateway.ts
@@ -1,7 +1,22 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 import { MessageWsService } from './message-ws.service';
+import { Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class MessageWsGateway {
+@WebSocketGateway({ cors: true })
+export class MessageWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   constructor(private readonly messageWsService: MessageWsService) {}
+
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log('Cliente conectado:', client.id);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('Cliente desconectado', client.id);
+  }
 }
